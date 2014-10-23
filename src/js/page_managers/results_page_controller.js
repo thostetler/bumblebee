f01d6e8553caa3922e112837a8e4a16d7bce6c55
Alex <aholachek@gmail.com>
2014-10-23 07:25:41 -0400

    placeholder

--- src/js/page_managers/results_page_controller.js
+++ src/js/page_managers/results_page_controller.js
@@ -33,6 +33,33 @@ define([
         var options = options || {};
         this.widgetDict = options.widgetDict;
 
+        //listen to window resize to figure out what we need to do with the 3rd column
+        //should this go here?
+
+        $(window).resize(function() {
+
+          if (this.$(".right-expand").css("display") == "none") {
+
+            //move third col to first col if it's not already there
+
+            if (!this.$("#results-left-column").find(".right-col-container").length){
+              this.$("#results-left-column").append(this.$(".right-col-container"))
+            }
+
+          }
+          else {
+
+            //move third col back into third col if it's not already there
+
+            if (!this.$("#results-right-column").find(".right-col-container").length){
+
+              this.$("#results-right-column").append(this.$(".right-col-container"))
+
+            }
+          }
+
+        });
+
       },
 
       template : threeColumnTemplate,
@@ -43,8 +70,11 @@ define([
 
         this.displayControlRow();
         this.displayFacets();
-        this.displayRightColumn();
+        //the order of the next two are important
+        // the right col checks for css value of expander buttons
+        //to decide where to render itself
         this.displayResultsList();
+        this.displayRightColumn();
       },
 
 
@@ -73,14 +103,26 @@ define([
       },
 
       displayRightColumn: function () {
+        //check for width-- if we are medium sized, append to third col
+        //else append to bottom of left col
+
         this.$(".right-col-container")
           .append(this.widgetDict.graphTabs.render().el);
 
-          if (Marionette.getOption(this, "debug")) {
-            this.$(".right-col-container")
-              .append(this.widgetDict.queryDebugInfo.render().el);
+        if (Marionette.getOption(this, "debug")) {
+          this.$(".right-col-container")
+            .append(this.widgetDict.queryDebugInfo.render().el);
 
-          }
+        }
+
+          if (this.$(".right-expand").css("display") == "none" ){
+
+          // it's a tablet or smaller
+
+          this.$("#results-left-column").append(this.$(".right-col-container"))
+
+        }
+
       },
 
       displayResultsList: function () {
@@ -89,7 +131,6 @@ define([
 
         this.$(".main-content-container")
           .append(this.widgetDict.results.render().el);
-
 
 
       },
@@ -111,54 +152,110 @@ define([
         "click .btn-expand" : "toggleColumns"
       },
 
+      returnBootstrapClasses :  function () {
+
+        var classes = this.classList
+        var toRemove = []
+        _.each(classes, function (c) {
+          if (c.indexOf("col-") !== -1) {
+            toRemove.push(c)
+          }
+        })
+        return toRemove.join(" ")
+      },
+
+      makeCenterFullWidth  : function(){
+
+        var $leftCol =  this.$(".s-results-left-column");
+        var $rightCol =  this.$(".s-results-right-column");
+
+        this.$("#results-middle-column")
+          .removeClass(this.returnBootstrapClasses)
+          .addClass("col-md-12 col-sm-12");
+
+        this.$(".btn-expand").addClass("btn-reversed");
+
+        $leftCol.children().width(0).fadeOut(500).hide();
+
+        $leftCol.addClass("hidden-col");
+
+        $rightCol.find(".right-col-container").width(0).hide(500);
+
+        $rightCol.addClass("hidden-col");
+
+      },
+
+      returnColWidthsToDefault : function(){
+
+        var $leftCol =  this.$(".s-results-left-column");
+        var $rightCol =  this.$(".s-results-right-column");
+
+        this.$("#results-middle-column")
+          .removeClass(this.returnBootstrapClasses)
+          .addClass("col-md-7 col-sm-8 ");
+
+        $leftCol.removeClass("hidden-col");
+        $leftCol.children().width('').fadeIn(500).children().show();
+
+        $rightCol.removeClass("hidden-col");
+        $rightCol.find(".right-col-container").width('').fadeIn(500);
+
+
+      },
+
+
       toggleColumns :function(e){
+
+        //this will remove all bootstrap column classes, it's used below
+
 
         var $t = $(e.currentTarget);
         var $leftCol =  this.$(".s-results-left-column");
         var $rightCol =  this.$(".s-results-right-column");
 
-        if ($t.hasClass("btn-upside-down")){
+        if ($t.hasClass("btn-reversed")){
 
-          $t.removeClass("btn-upside-down");
+          $t.removeClass("btn-reversed");
 
           if ($t.hasClass("left-expand")){
 
             $leftCol.removeClass("hidden-col")
-            $leftCol.find(".left-col-container").width('').fadeIn(500).children().show();
+            $leftCol.children().show(500);
 
           }
           else {
             $rightCol.removeClass("hidden-col");
 
-            $rightCol.find(".right-col-container").width('').fadeIn(500);
+            $rightCol.find(".right-col-container").show(500);
 
           }
 
           if (!$rightCol.hasClass("hidden-col") && !$leftCol.hasClass("hidden-col")){
             this.$("#results-middle-column")
-              .removeClass()
-              .addClass("col-sm-8 col-md-7")
+              .removeClass(this.returnBootstrapClasses)
+              .addClass("col-md-7 col-sm-8 ")
 
           }
           else if ($leftCol.hasClass("hidden-col")){
             this.$("#results-middle-column")
-              .removeClass()
-              .addClass("col-sm-")
-              .({"width": "75%"})
+              .removeClass(this.returnBootstrapClasses)
+              .addClass("col-md-9 col-sm-12")
+
           }
           else {
             this.$("#results-middle-column")
-              .css({"width":  "83.33333333%"})
+              .removeClass(this.returnBootstrapClasses)
+              .addClass("col-md-10 col-sm-8")
 
           }
 
         }
         else {
-          $t.addClass("btn-upside-down");
+          $t.addClass("btn-reversed");
 
           if ($t.hasClass("left-expand")){
 
-            $leftCol.find(".left-col-container").width(0).fadeOut(500).children().hide();
+            $leftCol.children().hide();
 
             $leftCol.addClass("hidden-col")
 
@@ -166,7 +263,7 @@ define([
           else {
             //expand to the right
 
-            $rightCol.find(".right-col-container").width(0).hide(500);
+            $rightCol.find(".right-col-container").hide();
 
             $rightCol.addClass("hidden-col");
 
@@ -175,19 +272,19 @@ define([
 
           if ($rightCol.hasClass("hidden-col") && $leftCol.hasClass("hidden-col")){
             this.$("#results-middle-column")
-              .removeClass()
+              .removeClass(this.returnBootstrapClasses)
               .addClass("col-md-12 col-sm-12")
 
           }
           else if ($rightCol.hasClass("hidden-col")){
             this.$("#results-middle-column")
-              // 58.33333 + 25
-              .css("width", "83.33333333%")
+              .removeClass(this.returnBootstrapClasses)
+              .addClass("col-md-10 col-sm-10")
           }
           else {
-            //58.33333 + 16.666666
             this.$("#results-middle-column")
-              .css("width", "75%")
+              .removeClass(this.returnBootstrapClasses)
+              .addClass("col-md-9 col-sm-12")
 
           }
 
@@ -251,6 +348,7 @@ define([
         }
 
       }
+
 
     });
 
