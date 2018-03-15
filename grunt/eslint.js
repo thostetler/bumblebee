c2c53ea0976492e972e82d4b3553751fa33a0331
Tim Hostetler <timothy.hostetler@cfa.harvard.edu>
2018-03-15 15:21:48 -0400

    update config

--- grunt/eslint.js
+++ grunt/eslint.js
@@ -8,10 +8,31 @@ module.exports = {
   options: {
     configFile: 'grunt/config/eslint.config.json'
   },
-  source: {
-    src: [
-      'src/**/*.js',
-      '!src/libs/**/*'
-    ]
+  apps: {
+    src: 'src/js/apps/**/*.js'
+  },
+  bugutils: {
+    src: 'src/js/bugutils/**/*.js'
+  },
+  components: {
+    src: 'src/js/components/**/*.js'
+  },
+  mixins: {
+    src: 'src/js/mixins/**/*.js'
+  },
+  modules: {
+    src: 'src/js/modules/**/*.js'
+  },
+  page_managers: {
+    src: 'src/js/page_managers/**/*.js'
+  },
+  services: {
+    src: 'src/js/services/**/*.js'
+  },
+  widgets: {
+    src: 'src/js/widgets/**/*.js'
+  },
+  wraps: {
+    src: 'src/js/wraps/**/*.js'
   }
 };
