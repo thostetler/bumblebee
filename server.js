95f49c38920fb9a6fff923b14d574defffcaea23
Tim Hostetler <timothy.hostetler@cfa.harvard.edu>
2018-07-12 13:08:15 -0400

    update

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-"use strict";
+/* eslint no-console: off */
 
 /**
  * WARNING: this is a DEVELOPMENT server; DO NOT USE it in production!
@@ -10,10 +10,13 @@
  * @type {exports}
  */
 
+
 const express = require('express');
 const path = require('path');
 const compression = require('compression');
 const proxy = require('http-proxy-middleware');
+
+const { process, __dirname } = this;
 const app = express();
 const targets = {
   dev: 'https://devapi.adsabs.harvard.edu',
@@ -34,12 +37,32 @@ let config = {
   proxy: {
     target: targets[process.env.TARGET || 'dev'],
     changeOrigin: true
-  }
+  },
+
+  // api latency
+  apiLatency: [0, 0]
 };
 
 if (process.env.SERVER_ENV === 'release') {
   config = Object.assign(config, { root: '/dist' });
 }
+
+// const delayer = function (min, max) {
+//   return function (req, res, next) {
+//     const time = Math.floor(Math.random() * (max - min + 1)) + min;
+//     console.log('Delaying request ', req.originalUrl, ' for ', time, ' ms');
+//     setTimeout(next, time);
+//     const end = res.end;
+//     res.end = function () {
+//       const args = arguments;
+//       setTimeout(function () {
+//         console.log('ending request');
+//         console.log(res.body);
+//         end.apply(res, args);
+//       }, time);
+//     };
+//   };
+// };
 
 // serve the static assets
 app.use(compression());
@@ -49,7 +72,7 @@ app.use('/node_modules', express.static(path.join(__dirname, '/node_modules')));
 app.use('/bower_components', express.static(path.join(__dirname, '/bower_components')));
 
 // proxy api calls to the api endpoint
-app.use(config.apiPath, proxy(config.proxy));
+app.use(config.apiPath, /* delayer.apply(null, config.apiLatency), */proxy(config.proxy));
 
 // start the server
 app.listen(8000, () => {
