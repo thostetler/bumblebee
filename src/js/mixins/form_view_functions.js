98c669260630e5e57f89ff269683719ef546c240
Tim Hostetler <timothy.hostetler@cfa.harvard.edu>
2019-03-22 16:31:58 -0400

    fix issue with settings forms

--- src/js/mixins/form_view_functions.js
+++ src/js/mixins/form_view_functions.js
@@ -22,6 +22,7 @@ define([
     // when someone clicks on submit button
     // parent views/controllers need to listen for "submit-form" event
     triggerSubmit: function (e) {
+      e.preventDefault();
       // (only show error messages if submit == true), so once user has unsuccessfully
       // submitted 1 time
       // don't need to reset because view will be disposed of after successful submit event
